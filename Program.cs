058258e1fca77442e96ef468be905f5ee6b399b7
Bárbara <babyr.oliveira@gmail.com>
2020-06-02 23:12:42 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Figgle;
+
+namespace NomeASCIIArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Olá usuário, digite o seu nome (sem acentos) e receba uma ASCII Art: ");
+            string nomeu = (Console.ReadLine()); //nomeu= nome do usuário.
+            
+            string recadoamavel = "Você é incrível, a maior e mais linda obra de arte <3";
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Write(FiggleFonts.FlowerPower.Render(nomeu));
+
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine(recadoamavel);
+        }
+    }
+}
